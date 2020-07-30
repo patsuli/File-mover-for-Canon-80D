3e4959babfa07b72cc4ea6f50638a23f4d98ce20
Berk Dagli <berkdagli1996@gmail.com>
2020-07-30 16:20:28 +0300

    päivämäärä kansion nimeksi

--- Cmover.py
+++ Cmover.py
@@ -2,55 +2,46 @@ import os
 import datetime
 
 
-filenames = os.listdir("G:/DCIM/100CANON")
+#filenames = os.listdir("G:/DCIM/100CANON")
 
 #lists of file types
 #video_types = ["mp4"]
 #picture_types = ["cr2", "jpg"]
-print (filenames)
+#print (filenames)
 
 #tee variable paiva joka on kyseinen päivä muodossa 30072020
 
 
 # Directory päivittyvä kansio nimi
-directory = paiva
 
 # Parent Directory path
 parent_dir = "F:/herra MSTKs"
 # Path
-path = os.path.join(parent_dir, directory)
+#path = os.path.join(parent_dir, directory)
 # Create the directory
 # 'GeeksForGeeks' in
 # '/home / User / Documents'
-os.mkdir(path)
-print("Directory '% s' created" % directory)
+#os.mkdir(path)
+#print("Directory '% s' created" % directory)
 
-# Directory
-directory = "Geeks"
 
-# Parent Directory path
-parent_dir = "D:/Pycharm projects"
+#päivämäärä kansion nimeksi
+date = datetime.datetime.now()
+print(type(date))
+if int(date.month)<10:
+	month = "0" + str(date.month)
+paiva = str(date.day) + month + str(date.year)
+print(paiva)
 
-# mode
-mode = 0o666
-
-# Path
-path = os.path.join(parent_dir, directory)
-
-# Create the directory
-# 'GeeksForGeeks' in
-# '/home / User / Documents'
-# with mode 0o666
-os.mkdir(path, mode)
-print("Directory '% s' created" % directory)
-
+directory = paiva
 
 #tee kansiot ennen looppia
 pictures = []
 videos = []
-
+'''
 for i in filenames:
     if "jpg" in i or "cr2" in i:
     	pictures.append(i)
     else:
     	videos.append(i)
+'''   	
