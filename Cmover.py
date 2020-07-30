772475abdfed3bf6f6f606a36613b6bd565908ed
Patrik Hiltunen <patrikki00@outlook.com>
2020-07-30 15:52:17 +0300

    lukee kortin tiedostot

--- Cmover.py
+++ Cmover.py
@@ -1,2 +1,15 @@
-print ("Hello squirrel fuckers")
-print ("asdfg")
+import os
+import datetime
+
+
+filenames = os.listdir("G:/DCIM/100CANON")
+
+#lists of file types
+#video_types = ["mp4"]
+#picture_types = ["cr2", "jpg"]
+print (filenames)
+
+#tee kansiot ennen looppia
+
+for i in filenames:
+    if "jpg" in i or "cr2" in i:
